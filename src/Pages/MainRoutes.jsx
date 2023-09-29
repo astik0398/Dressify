5cc04cf70698a558dcd1247690a8374dc8a0b8d9
Kumar Astik <kumarastik0398@gmail.com>
2023-09-29 16:53:27 +0530

    Day-3-fw26_050
    pushed

--- src/Pages/MainRoutes.jsx
+++ src/Pages/MainRoutes.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { Homepage } from "../Pages/Homepage";
 import Product from "../Components/Product";
+import Signin from "../Components/Signin";
+import Signup from "../Components/Signup";
+import Admin from "../Components/Admin";
 export const MainRoutes = () => {
   return (
     <>
@@ -10,6 +13,10 @@ export const MainRoutes = () => {
         <Route path="*" element={<h1>Page is Not found</h1>}></Route>
 
         <Route path="/products" element={<Product />} />
+
+        <Route path='/signin' element={<Signin/>}/>
+        <Route path='/signup' element={<Signup/>}/>
+        <Route path='/admin' element={<Admin/>}/>
       </Routes>
     </>
   );
