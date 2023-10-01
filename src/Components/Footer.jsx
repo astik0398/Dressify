bbb42f2412b1f94fc7a58b9cfe7919dbacb6b27f
Sonika Kanojia <115460678+Sonika2223Kanojia@users.noreply.github.com>
2023-10-01 08:51:25 +0530

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,74 +1,94 @@
-Skip to content
-astik0398
-/
-ambiguous-oven-8467
 
-Type / to search
+import {
+  HStack,
+  Heading,
+  Link,
+  VStack,
+  Box,
+  Container,
+  Stack,
+  Text,
+  Button,
+  Image,
+} from "@chakra-ui/react";
+import { FaTwitter, FaYoutube, FaInstagram } from "react-icons/fa";
+import React from "react";
 
-Code
-Issues
-Pull requests
-1
-Actions
-Projects
-Security
-Insights
-Breadcrumbsambiguous-oven-8467/src/Components
-/
-Footer.jsx
-in
-main
-
-Edit
-
-Preview
-Indent mode
-
-Spaces
-Indent size
-
-2
-Line wrap mode
-
-No wrap
-Editing Footer.jsx file contents
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-import logo from './logo.svg';
-import './App.css';
-import {Navbar}  from "./Components/Navbar";
-import {Footer}  from "./Components/Footer";
-import { MainRoutes } from "./Pages/MainRoutes";
-
-function App() {
-  //Do not modify anything in this file
+export const Footer = () => {
   return (
-    <div className="App">
-      <Navbar />
-      <MainRoutes />
-      <Footer/>
-      </div>
+    <Box padding={"30px 30px 10px 30px"} bg={"black"} color="white">
+      <Stack
+        direction={{ base: "column", md: "row", lg: "row" }}
+        justifyContent={"space-around"}
+        alignItems={"flex-start"}
+      >
+        <VStack alignItems={"flex-start"}>
+          <Heading as={"h3"} size={"md"}>
+            Company
+          </Heading>
+          <Link>About Us</Link>
+          <Link>Blog</Link>
+          <Link>Collection</Link>
+          <Link>Contact Us</Link>
+        </VStack>
+        <VStack alignItems={"flex-start"}>
+          <Heading as={"h3"} size={"md"}>
+            Support
+          </Heading>
+          <Link>Help Center</Link>
+          <Link>Safety Center</Link>
+          <Link>Community Guidelines</Link>
+        </VStack>
+        <VStack alignItems={"flex-start"}>
+          <Heading as={"h3"} size={"md"}>
+            Legal
+          </Heading>
+          <Link>Cooking Policy</Link>
+          <Link>Privacy Policy</Link>
+          <Link>Terms of Service</Link>
+          <Link>Law Enforcement</Link>
+        </VStack>
+        <VStack alignItems={"flex-start"}>
+          <Heading as={"h3"} size={"md"}>
+            Install App
+          </Heading>
+          <Image
+            _hover={{cursor:"pointer"}}
+            src={
+              "https://www.freepnglogos.com/uploads/play-store-logo-png/play-store-logo-nisi-filters-australia-11.png"
+            }
+            width={"200px"}
+            objectFit={"cover"}
+          />
+
+          <Image
+            _hover={{cursor:"pointer"}}
+            src={
+              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRic3NEzq52LhkWEYfxZA6YAPTT3oYQ_6C27Q&usqp=CAU"
+            }
+            width={"200px"}
+            objectFit={"cover"}
+          />
+        </VStack>
+      </Stack>
+      <Stack
+        borderTopWidth={1}
+        borderStyle={"solid"}
+        marginTop={"10px"}
+        paddingTop={"10px"}
+        justifyContent="space-around"
+        alignItems={{ base: "flex-start", md: "center", lg: "center" }}
+        direction={{ base: "column", md: "row", lg: "row" }}
+      >
+        <Text>© 2023 spendwise, All rights reserved</Text>
+        <Stack direction={"row"} spacing={6}>
+          <FaTwitter className="footer-icon" />
+          <FaYoutube className="footer-icon" />
+          <FaInstagram className="footer-icon" />
+        </Stack>
+      </Stack>
+    </Box>
   );
-}
+};
 
-export default App;
-
-Use Control + Shift + m to toggle the tab key moving focus. Alternatively, use esc then tab to move to the next interactive element on the page.
-Editing ambiguous-oven-8467/src/Components/Footer.jsx at main · astik0398/ambiguous-oven-8467
+export default Footer;
