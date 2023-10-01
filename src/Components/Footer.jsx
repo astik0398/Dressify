6ff3c25e17a45393d874943dd3e0dac166b43d3e
Sonika Kanojia <115460678+Sonika2223Kanojia@users.noreply.github.com>
2023-10-01 08:50:01 +0530

    Update Footer.jsx

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,74 @@
+Skip to content
+astik0398
+/
+ambiguous-oven-8467
+
+Type / to search
+
+Code
+Issues
+Pull requests
+1
+Actions
+Projects
+Security
+Insights
+Breadcrumbsambiguous-oven-8467/src/Components
+/
+Footer.jsx
+in
+main
+
+Edit
+
+Preview
+Indent mode
+
+Spaces
+Indent size
+
+2
+Line wrap mode
+
+No wrap
+Editing Footer.jsx file contents
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+import logo from './logo.svg';
+import './App.css';
+import {Navbar}  from "./Components/Navbar";
+import {Footer}  from "./Components/Footer";
+import { MainRoutes } from "./Pages/MainRoutes";
+
+function App() {
+  //Do not modify anything in this file
+  return (
+    <div className="App">
+      <Navbar />
+      <MainRoutes />
+      <Footer/>
+      </div>
+  );
+}
+
+export default App;
+
+Use Control + Shift + m to toggle the tab key moving focus. Alternatively, use esc then tab to move to the next interactive element on the page.
+Editing ambiguous-oven-8467/src/Components/Footer.jsx at main · astik0398/ambiguous-oven-8467
